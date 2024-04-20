e96fd7092eb0621d0f5510245b4dcecd3154f0f7
Abdiwali-eid <cabdiwalibashir@gmail.com>
2024-04-20 16:56:30 +0300

    tijaabo

--- web/src/components/homePage/HeroSection.js
+++ web/src/components/homePage/HeroSection.js
@@ -13,7 +13,7 @@ function HeroSection() {
       <div className="container">
         <div className="hero__wrapper">
           <div className="left">
-            <h1 className="hero__heading">waydiiya kuwa cilmiga leh hadii aydan aqoon</h1>
+            <h1 className="hero__heading">waydiiya kuwa cilmiga u leh hadii aydan aqoon</h1>
             <ParagraphText className="hero__text">
               website-kaan waxaad ka heli kartaa su'aalo iyo jawaabo diini ah,sidoo kale waxaad waydiin kartaa su'aalaha diiniga ah oo ay kaaga soo jawaabayaan culimo aqoon leh.
             </ParagraphText>
